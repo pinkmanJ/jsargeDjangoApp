571e49bd595260217013433cfc309a5268d6d4be
Jesse Sargent <mountaneer12@gmail.com>
2021-09-16 09:42:23 -0700

    Overrode get_queryset() - PlaylistSerializer to return queryset for user

--- mysite/music/views.py
+++ mysite/music/views.py
@@ -1,3 +1,4 @@
+from django.db import models
 from music.serializers import SongSerializer
 from music.serializers import PlaylistSerializer
 from django.db.models.query import QuerySet
@@ -14,10 +15,6 @@ from music.models import Song, Playlist
 # Create your views here.
 def index(request):
     return HttpResponse("Hello, world. You're at the music index.")
-    # we could pull data from database
-    # transform data and send it 
-    # serve template (which is an HTML page)
-    # send out email
 
 def songs(request):
     list_of_strings = Song.objects.all().values_list("name", flat=True)
@@ -29,18 +26,24 @@ def songs(request):
     #return HttpResponse( "".join(list_of_strings))
     
 
-def detail(request, artist_id):
-    return HttpResponse("You're looking at artist %s." % artist_id)
+# def detail(request, artist_id):
+#     return HttpResponse("You're looking at artist %s." % artist_id)
 
-def getAlbums(request, artist_id):
-    return HttpResponse("You're looking at artist %s albums" % artist_id)    
+# def getAlbums(request, artist_id):
+#     return HttpResponse("You're looking at artist %s albums" % artist_id)    
 
-def getSongs(request, artist_id):
-    return HttpResponse("You're looking at artist %s songs" % artist_id)
+# def getSongs(request, artist_id):
+#     return HttpResponse("You're looking at artist %s songs" % artist_id)
 
 class PlaylistViewSet(ModelViewSet):          
     queryset = Playlist.objects.all()
     serializer_class = PlaylistSerializer
+
+    # override get_queryset if you need to return different sets based on request
+    # this makes it so that a user can only view Playlists they created
+    def get_queryset(self):
+        user = self.request.user
+        return Playlist.objects.filter(user=user)
 
 class SongViewSet(ModelViewSet):
     queryset = Song.objects.all()
